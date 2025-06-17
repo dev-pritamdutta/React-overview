9ec401e78e3e096ab6b048f3d93c4c038a1ecaff
pritam dutta <webdevpritam7498@gmail.com>
2025-06-17 21:19:55 +0600

    basic state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  const [mymoney, setMymoney] = useState(100);
+
+  const handleMoney = () => {
+    setMymoney(mymoney + 100);
+    console.log("Money updated:", mymoney);
+  };
+
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h3>my money: {mymoney}</h3>
+      <button onClick={() => {handleMoney()}} className="p-3 bg-amber-200 my-16">
+        Add money
+      </button>
     </div>
   );
 };
